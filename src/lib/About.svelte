7e856a61d7aca8a2cbe0034e345cba16a5c74b9b
Leo Deng <leo7deng@gmail.com>
2024-01-16 00:40:56 -0500

    create about section

---
+++ src/lib/About.svelte
@@ -0,0 +1,39 @@
+<div class="about-me">
+	<h1>About Me</h1>
+	<h4>
+		I'm a student interested in learning many skills in software development, aiming to become a
+		more well rounded engineer. As a generalist, I always thought I would never master a skill, but
+		now I have learned to lean into my strengths of being a fast learner to succeed in any field.
+	</h4>
+	<h4>
+		I started my journey into computer science with machine learning, hoping to solve major issues
+		in the healthcare industry. As I got involved with research, I realized the many blind spots I
+		had in coding in general. I tried my skill in competitive coding where I further learned I knew
+		nothing about web development. After developing frontend and backend web projects, I realized
+		the impossible task of learning everything about computer science. This website is to share that
+		impossible journey.
+	</h4>
+	<h4>
+		Outside of learning, I still have many responsibilities to my life in bodybuilding, trying all
+		kinds of food, and travelling the world.
+	</h4>
+	<h4>If you want to chat, please feel free to reach out!</h4>
+</div>
+
+<style lang="scss">
+.about-me {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+
+    h1 {
+        grid-column-start: 4;
+        margin-bottom: 0;
+    }
+
+    h4 {
+        grid-column-start: 4;
+        line-height: 25px;
+        font-weight: 600;
+    }
+}
+</style>
